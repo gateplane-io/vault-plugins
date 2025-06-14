5d23c2e5b4e1a73a74d7035fdfed00e405285d0c
John Torakis <john.torakis@gmail.com>
2025-06-14 18:38:45 +0300

    feat: add Release workflow

---
+++ scripts/plugin_versions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+plugins="$(ls cmd/ | tr '\n' ' ')"
+
+echo "# Plugin codebases: [$plugins]"
+base_version=$(git describe --tags --match "base/*" --abbrev=0 | cut -d/ -f2)
+echo "# Base - base/${base_version}"
+for PLUGIN in $plugins; do
+	plugin_version=$(git describe --tags --match "$PLUGIN/*" --abbrev=0  2>/dev/null| cut -d/ -f2)
+
+	if [ ! -n "$plugin_version" ]; then
+		echo "# Plugin '${PLUGIN}' does not have a tag of ${PLUGIN}/x.x.x"
+		continue
+	fi
+	tag="v$plugin_version-base.$base_version"
+	echo "# ${PLUGIN} - ${PLUGIN}/${plugin_version}"
+	echo "export VERSION_${PLUGIN/-/_}=$tag"
+
+done;
